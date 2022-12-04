bfe466aa68ae70568edf77f5f7d0084cdd7fdbe2
jochongs <joch2712@naver.com>
2022-12-04 10:00:53 +0000

    modify post_save_es.js with saving post_idx to es._id

---
+++ schedule/account_save_es.js
@@ -0,0 +1,54 @@
+const elastic = require('elasticsearch');
+const { Client } = require('pg');
+require('dotenv').config({ path : '../.env'});
+const pgConfig = {
+    user : process.env.POSTGRE_USER,
+    password : process.env.POSTGRE_PASSWORD,
+    host : process.env.POSTGRE_HOST,
+    database : process.env.POSTGRE_DATABASE,
+    post : process.env.POSTGRE_POST
+}
+
+
+
+const saveAccountDataToEs = async () => {
+    try{
+        //CONNECT pg
+        const pgClient = new Client(pgConfig);
+        await pgClient.connect();
+
+        //CONNECT es
+        const esClient = new elastic.Client({
+            node : "http://localhost:9200"
+        });
+        
+        //SELECT 
+        const selectAccountDataSql = 'SELECT * FROM backend.account';
+        const accountResult = await pgClient.query(selectAccountDataSql);
+        const accountDataArray = accountResult.rows;
+
+        for(let i = 0; i < accountDataArray.length; i++){
+            //prepare data
+            const accountData = accountDataArray[i];
+
+            //POST data to elasticsearch
+            const response = await esClient.index({
+                index : 'account',
+                _id : accountData.id,
+                body : {
+                    id : accountData.id,
+                    name : accountData.name,
+                    nickname : accountData.nickname,
+                    authority : accountData.authority,
+                    login_type : accountData.login_type,
+                }
+            });
+
+            console.log(response);
+        }
+    }catch(err){
+        console.log(err);
+    }
+}
+
+saveAccountDataToEs();
--- schedule/post_save_es.js
+++ schedule/post_save_es.js
@@ -35,20 +35,25 @@ const savePostDataToEs = async () => {
             const selectImgPathSql = 'SELECT * FROM backend.post_img_mapping WHERE post_idx = $1';
             const imgPathArray = await pgClient.query(selectImgPathSql, [postData.post_idx]);
 
-            //console.log(imgPathArray.rows);
+            //SELECT commnet 
+            const selectCommentSql = 'SELECT * FROM backend.comment WHERE post_idx = $1';
+            const selectCommentQuery = await pgClient.query(selectCommentSql, [postData.post_idx]);
+            const commentDataArray = selectCommentQuery.rows;
 
-            console.log(imgPathArray.rows.map(imgPathData => imgPathData.img_path));
+            console.log(commentDataArray.map((comment) => {return {comment_idx : comment.comment_idx, comment_author : comment.comment_author, comment_contents : comment.comment_contents, comment_date : comment.comment_date}}));
 
             //POST data to elasticsearch
             const response = await esClient.index({
                 index : 'post',
+                id : postData.post_idx,
                 body : {
                     post_idx : postData.post_idx,
                     post_title : postData.post_title,
                     post_contents : postData.post_contents,
                     post_author : postData.post_author,
                     post_date : new Date(postData.post_date).getTime(),
-                    post_img_path : imgPathArray.rows.map((imgPathData) => imgPathData.img_path)
+                    post_img_path : imgPathArray.rows.map((imgPathData) => imgPathData.img_path),
+                    comment : commentDataArray.map((comment) => {return {comment_idx : comment.comment_idx, comment_author : comment.comment_author, comment_contents : comment.comment_contents, comment_date : comment.comment_date}})
                 }
             });
 
