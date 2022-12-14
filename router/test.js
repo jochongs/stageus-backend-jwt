0a7597c3f778ebbeb5d24d76d2701ffdb742efd3
jochongs <joch2712@naver.com>
2022-12-14 11:05:42 +0000

    readme

--- router/test.js
+++ router/test.js
@@ -1,9 +1,9 @@
+//시퀄라이즈 테스트 코드 ( 사용하지 않을 예정 )
 const router = require('express').Router();
 const pgConfig = require('../config/pg_config');
 
 const { Sequelize } = require('sequelize');
 const sequelize = require('../module/sequelize');
-
 
 (async ()=>{
     try{
